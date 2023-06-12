fbea2372687ee692cdef0fcb6c2f44b3d214fdeb
raviiitejaaa9 <63893669+raviiitejaaa9@users.noreply.github.com>
2023-06-12 17:42:33 +0530

    Update index.js

--- src/components/Stopwatch/index.js
+++ src/components/Stopwatch/index.js
@@ -1,35 +1,74 @@
 // Write your code here
+
 import {Component} from 'react'
 import './index.css'
 
+const initialState = {
+  timerElapsedInMinutes: 0,
+  timeElapsedInSeconds: 0,
+  isStarted: false,
+}
+
 class Stopwatch extends Component {
-  state = {
-    count: 0,
-    isStarted: false,
+  state = initialState
+
+  componentWillUnmount() {
+    this.clearTimerInterval()
+  }
+
+  clearTimeInterval = () => clearInterval(this.intervalId)
+
+  incrementTimeElapsedInSeconds = () => {
+    this.setState(prevState => ({
+      timeElapsedInSeconds: prevState.timeElapsedInSeconds + 1,
+    }))
   }
 
   onStart = () => {
-    console.log('start Triggered')
-    this.setState(prevState => ({isStarted: !prevState.isStarted}))
+    console.log('Start Triggered')
+    const {isStarted} = this.state
+    if (isStarted === false) {
+      this.intervalId = setInterval(this.incrementTimeElapsedInSeconds, 1000)
+    }
+    this.setState(prevState => ({
+      isStarted: !prevState.isStarted,
+    }))
   }
 
   onStop = () => {
-    console.log('start Triggered')
+    console.log('Stop Triggered')
+    const {isStarted} = this.state
+    if (isStarted) {
+      this.clearTimeInterval()
+    }
+    this.setState(prevState => ({isStarted: !prevState.isStarted}))
   }
 
   onReset = () => {
-    console.log('start Triggered')
+    console.log('Reset Triggered')
+    this.clearTimeInterval()
+    this.setState(initialState)
+  }
+
+  getElapsedSecondsInTimeFormat = () => {
+    const {timeElapsedInSeconds} = this.state
+    const totalElapsedSeconds = timeElapsedInSeconds
+    const minutes = Math.floor(totalElapsedSeconds / 60)
+    const seconds = Math.floor(totalElapsedSeconds % 60)
+    const stringifiedMinutes = minutes > 9 ? minutes : `0${minutes}`
+    const stringifiedSeconds = seconds > 9 ? seconds : `0${seconds}`
+
+    return `${stringifiedMinutes}:${stringifiedSeconds}`
   }
 
   render() {
-    const {count, isStarted} = this.state
+    // const {isStarted} = this.state
 
-    console.log(isStarted)
+    // console.log(isStarted)
 
     return (
       <div className="app-container">
         <h1> Stopwatch </h1>
-        <p> {count} </p>
         <div className="card-container">
           <div className="card-head">
             <img
@@ -39,7 +78,7 @@ class Stopwatch extends Component {
             />
             <p>Timer</p>
           </div>
-          <h1> 00:00 </h1>
+          <h1> {this.getElapsedSecondsInTimeFormat()} </h1>
           <div className="buttons-container">
             <button className="start-btn" onClick={this.onStart} type="button">
               Start
